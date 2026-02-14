a2dfc13045298dd4cf6250b210f09378aeecc4ab
vadlamuriaravind <vadlamuriaravind@gmail.com>
2026-02-14 16:57:00 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,62 @@
-from pyspark.cloudpickle.cloudpickle import *  # noqa
-from pyspark.cloudpickle.cloudpickle_fast import CloudPickler, dumps, dump  # noqa
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Conform to the convention used by python serialization libraries, which
-# expose their Pickler subclass at top-level under the  "Pickler" name.
-Pickler = CloudPickler
+"""
+PySpark exceptions.
+"""
+from pyspark.errors.exceptions.base import (  # noqa: F401
+    PySparkException,
+    AnalysisException,
+    TempTableAlreadyExistsException,
+    ParseException,
+    IllegalArgumentException,
+    ArithmeticException,
+    ArrayIndexOutOfBoundsException,
+    DateTimeException,
+    NumberFormatException,
+    StreamingQueryException,
+    QueryExecutionException,
+    PythonException,
+    UnknownException,
+    SparkRuntimeException,
+    SparkUpgradeException,
+    PySparkTypeError,
+    PySparkValueError,
+    PySparkAttributeError,
+)
 
-__version__ = '2.2.1'
+
+__all__ = [
+    "PySparkException",
+    "AnalysisException",
+    "TempTableAlreadyExistsException",
+    "ParseException",
+    "IllegalArgumentException",
+    "ArithmeticException",
+    "ArrayIndexOutOfBoundsException",
+    "DateTimeException",
+    "NumberFormatException",
+    "StreamingQueryException",
+    "QueryExecutionException",
+    "PythonException",
+    "UnknownException",
+    "SparkRuntimeException",
+    "SparkUpgradeException",
+    "PySparkTypeError",
+    "PySparkValueError",
+    "PySparkAttributeError",
+]
