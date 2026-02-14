23a6bfe1500a80f5bccec0b4f0a8bfa4df2138d1
vadlamuriaravind <vadlamuriaravind@gmail.com>
2026-02-14 17:09:20 +0530

    Add files via upload

--- common.py
+++ common.py
@@ -15,160 +15,62 @@
 # limitations under the License.
 #
 
-from typing import Any, Callable, TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from pyspark.mllib._typing import C, JavaObjectOrPickleDump
-
-import py4j.protocol
-from py4j.protocol import Py4JJavaError
-from py4j.java_gateway import JavaObject
-from py4j.java_collections import JavaArray, JavaList
-
-import pyspark.context
-from pyspark import RDD, SparkContext
-from pyspark.serializers import CPickleSerializer, AutoBatchedSerializer
-from pyspark.sql import DataFrame, SparkSession
-
-# Hack for support float('inf') in Py4j
-_old_smart_decode = py4j.protocol.smart_decode
-
-_float_str_mapping = {
-    "nan": "NaN",
-    "inf": "Infinity",
-    "-inf": "-Infinity",
-}
+def memory_usage(f):
+    return f(
+        "memory_usage",
+        reason="Unlike pandas, most DataFrames are not materialized in memory in Spark "
+        "(and pandas-on-Spark), and as a result memory_usage() does not do what you intend it "
+        "to do. Use Spark's web UI to monitor disk and memory usage of your application.",
+    )
 
 
-def _new_smart_decode(obj: Any) -> str:
-    if isinstance(obj, float):
-        s = str(obj)
-        return _float_str_mapping.get(s, s)
-    return _old_smart_decode(obj)
+def array(f):
+    return f(
+        "array",
+        reason="If you want to collect your data as an NumPy array, use 'to_numpy()' instead.",
+    )
 
 
-py4j.protocol.smart_decode = _new_smart_decode
+def to_pickle(f):
+    return f(
+        "to_pickle",
+        reason="For storage, we encourage you to use Delta or Parquet, instead of Python pickle "
+        "format.",
+    )
 
 
-_picklable_classes = [
-    "LinkedList",
-    "SparseVector",
-    "DenseVector",
-    "DenseMatrix",
-    "Rating",
-    "LabeledPoint",
-]
+def to_xarray(f):
+    return f(
+        "to_xarray",
+        reason="If you want to collect your data as an NumPy array, use 'to_numpy()' instead.",
+    )
 
 
-# this will call the MLlib version of pythonToJava()
-def _to_java_object_rdd(rdd: RDD) -> JavaObject:
-    """Return a JavaRDD of Object by unpickling
-
-    It will convert each Python object into Java object by Pickle, whenever the
-    RDD is serialized in batch or not.
-    """
-    rdd = rdd._reserialize(AutoBatchedSerializer(CPickleSerializer()))
-    assert rdd.ctx._jvm is not None
-    return rdd.ctx._jvm.org.apache.spark.mllib.api.python.SerDe.pythonToJava(rdd._jrdd, True)
+def to_list(f):
+    return f(
+        "to_list",
+        reason="If you want to collect your data as an NumPy array, use 'to_numpy()' instead.",
+    )
 
 
-def _py2java(sc: SparkContext, obj: Any) -> JavaObject:
-    """Convert Python object into Java"""
-    if isinstance(obj, RDD):
-        obj = _to_java_object_rdd(obj)
-    elif isinstance(obj, DataFrame):
-        obj = obj._jdf
-    elif isinstance(obj, SparkContext):
-        obj = obj._jsc
-    elif isinstance(obj, list):
-        obj = [_py2java(sc, x) for x in obj]
-    elif isinstance(obj, JavaObject):
-        pass
-    elif isinstance(obj, (int, float, bool, bytes, str)):
-        pass
-    else:
-        data = bytearray(CPickleSerializer().dumps(obj))
-        assert sc._jvm is not None
-        obj = sc._jvm.org.apache.spark.mllib.api.python.SerDe.loads(data)
-    return obj
+def tolist(f):
+    return f(
+        "tolist",
+        reason="If you want to collect your data as an NumPy array, use 'to_numpy()' instead.",
+    )
 
 
-def _java2py(sc: SparkContext, r: "JavaObjectOrPickleDump", encoding: str = "bytes") -> Any:
-    if isinstance(r, JavaObject):
-        clsName = r.getClass().getSimpleName()
-        # convert RDD into JavaRDD
-        if clsName != "JavaRDD" and clsName.endswith("RDD"):
-            r = r.toJavaRDD()
-            clsName = "JavaRDD"
-
-        assert sc._jvm is not None
-
-        if clsName == "JavaRDD":
-            jrdd = sc._jvm.org.apache.spark.mllib.api.python.SerDe.javaToPython(r)
-            return RDD(jrdd, sc)
-
-        if clsName == "Dataset":
-            return DataFrame(r, SparkSession._getActiveSessionOrCreate())
-
-        if clsName in _picklable_classes:
-            r = sc._jvm.org.apache.spark.mllib.api.python.SerDe.dumps(r)
-        elif isinstance(r, (JavaArray, JavaList)):
-            try:
-                r = sc._jvm.org.apache.spark.mllib.api.python.SerDe.dumps(r)
-            except Py4JJavaError:
-                pass  # not pickable
-
-    if isinstance(r, (bytearray, bytes)):
-        r = CPickleSerializer().loads(bytes(r), encoding=encoding)
-    return r
+def __iter__(f):
+    return f(
+        "__iter__",
+        reason="If you want to collect your data as an NumPy array, use 'to_numpy()' instead.",
+    )
 
 
-def callJavaFunc(
-    sc: pyspark.context.SparkContext, func: Callable[..., "JavaObjectOrPickleDump"], *args: Any
-) -> Any:
-    """Call Java Function"""
-    java_args = [_py2java(sc, a) for a in args]
-    return _java2py(sc, func(*java_args))
-
-
-def callMLlibFunc(name: str, *args: Any) -> Any:
-    """Call API in PythonMLLibAPI"""
-    sc = SparkContext.getOrCreate()
-    assert sc._jvm is not None
-    api = getattr(sc._jvm.PythonMLLibAPI(), name)
-    return callJavaFunc(sc, api, *args)
-
-
-class JavaModelWrapper:
-    """
-    Wrapper for the model in JVM
-    """
-
-    def __init__(self, java_model: JavaObject):
-        self._sc = SparkContext.getOrCreate()
-        self._java_model = java_model
-
-    def __del__(self) -> None:
-        assert self._sc._gateway is not None
-        self._sc._gateway.detach(self._java_model)
-
-    def call(self, name: str, *a: Any) -> Any:
-        """Call method of java_model"""
-        return callJavaFunc(self._sc, getattr(self._java_model, name), *a)
-
-
-def inherit_doc(cls: "C") -> "C":
-    """
-    A decorator that makes a class inherit documentation from its parents.
-    """
-    for name, func in vars(cls).items():
-        # only inherit docstring for public functions
-        if name.startswith("_"):
-            continue
-        if not func.__doc__:
-            for parent in cls.__bases__:
-                parent_func = getattr(parent, name, None)
-                if parent_func and getattr(parent_func, "__doc__", None):
-                    func.__doc__ = parent_func.__doc__
-                    break
-    return cls
+def duplicated(f):
+    return f(
+        "duplicated",
+        reason="'duplicated' API returns np.ndarray and the data size is too large."
+        "You can just use DataFrame.deduplicated instead",
+    )
